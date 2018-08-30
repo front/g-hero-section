14ab71dedc9fc984ef5578ced0aa718dd6efc5ac
Élio Cró <elio@front.no>
2018-08-30 15:42:48 +0100

    Register separate category for the block

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 
-import { blocks } from '@frontkom/gutenberg-js';
+import { blocks, data, i18n } from '@frontkom/gutenberg-js';
 const { registerBlockType } = blocks;
+const { dispatch, select } = data;
+const { __ } = i18n;
 
+// Import each block
 import * as hero from './hero-section';
 
 
-// Export register block functionality
-export function registerBlocks (category) {
-  registerBlockType(`${category}/${hero.name}`, { category, ...hero.settings });
+// Category name and slug
+const category = {
+  slug: 'cloudblocks', // needs to match the css class of the block container: ".wp-block-cloudblocks-[block-name]"
+  title: __('Gutenberg-Cloud Blocks'),
+};
+
+
+// Register the new category and blocks
+export function registerBlocks () {
+  // Add the new category to the list
+  const currentCategories = select('core/blocks').getCategories().filter(item => item.slug !== category.slug);
+  dispatch('core/blocks').setCategories([ category, ...currentCategories ]);
+
+  // Register each block
+  registerBlockType(`${category.slug}/${hero.name}`, { category: category.slug, ...hero.settings });
 }
