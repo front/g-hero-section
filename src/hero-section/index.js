5ef80791ec0dbeb269b19b667d57ab9c76090663
Sofia Sousa <csofiamsousa@gmail.com>
2018-09-10 11:07:09 +0100

    tp26842 feat: Add id to Content width field tp26842 feat: Add Fragment in order to follow core-blocks structure

--- src/hero-section/index.js
+++ src/hero-section/index.js
@@ -4,7 +4,7 @@
  * External dependencies
  */
 import React from 'react';
-import { i18n, components, editor } from 'wp';
+import { element, i18n, components, editor } from 'wp';
 
 /**
  * Internal dependencies
@@ -12,10 +12,10 @@ import { i18n, components, editor } from 'wp';
 import './style.scss';
 import image from './iphone.svg';
 
+const { Fragment } = element;
 const { __ } = i18n;
 const { PanelBody, BaseControl, RangeControl, IconButton, Toolbar, SelectControl } = components;
 const { InnerBlocks, InspectorControls, PanelColorSettings, MediaUpload, BlockControls } = editor;
-
 
 const TEMPLATE = [
   ['core/heading', {
@@ -34,7 +34,6 @@ const TEMPLATE = [
     url: 'https://github.com/front/gutenberg-js',
   }],
 ];
-
 
 export const name = 'hero-section';
 
@@ -120,102 +119,109 @@ export const settings = {
       });
     };
 
-    return [
-      <div className={ className } style={ containerStyle } { ...backgroundImageData }>
-        <div className="bg-overlay" style={ overlayStyle }></div>
-        <section className={ `image-${imageLayout}` } style={ wrapperStyle }>
-          <main>
-            <InnerBlocks template={ TEMPLATE } templateLock={ false } />
-          </main>
-          { imageLayout && <div className="image-feature">
+    return (
+      <Fragment>
+        <div className={ className } style={ containerStyle } { ...backgroundImageData }>
+          <div className="bg-overlay" style={ overlayStyle }></div>
+          <section className={ `image-${imageLayout}` } style={ wrapperStyle }>
+            <main>
+              <InnerBlocks template={ TEMPLATE } templateLock={ false } />
+            </main>
+            { imageLayout && <div className="image-feature">
+              <MediaUpload type="image"
+                onSelect={ media => onSelectImage(media, 'imageUrl') } render={({ open }) => (
+                  <IconButton className="components-toolbar__control" label={ __('Edit image') }
+                    icon="edit" onClick={ open } />
+                ) }
+              />
+              <img src={ imageUrl } { ...imageUrlData } />
+            </div> }
+          </section>
+        </div>
+
+        { backgroundType === 'image' && <BlockControls>
+          <Toolbar>
             <MediaUpload type="image"
-              onSelect={ media => onSelectImage(media, 'imageUrl') } render={({ open }) => (
+              onSelect={ media => onSelectImage(media, 'backgroundImage') } render={ ({ open }) => (
                 <IconButton className="components-toolbar__control" label={ __('Edit image') }
                   icon="edit" onClick={ open } />
               ) }
             />
-            <img src={ imageUrl } { ...imageUrlData } />
-          </div> }
-        </section>
-      </div>,
+          </Toolbar>
+        </BlockControls> }
 
-      backgroundType === 'image' && <BlockControls>
-        <Toolbar>
-          <MediaUpload type="image"
-            onSelect={ media => onSelectImage(media, 'backgroundImage') } render={ ({ open }) => (
-              <IconButton className="components-toolbar__control" label={ __('Edit image') }
-                icon="edit" onClick={ open } />
-            ) }
-          />
-        </Toolbar>
-      </BlockControls>,
+        <InspectorControls>
+          <PanelBody title={ __('Block Settings') }>
+            <BaseControl label={ __('Content Width in pixels') } id="block-hero-section-content-width-input">
+              <input
+                type="number"
+                id="block-hero-section-content-width-input"
+                value={ contentWidth }
+                onChange={ ev => setAttributes({ contentWidth: parseInt(ev.target.value, 10) }) }
+                step="10"
+              />
+            </BaseControl>
 
-      <InspectorControls>
-        <PanelBody title={ __('Block Settings') }>
-          <BaseControl label={ __('Content Width in pixels') }>
-            <input type="number" value={ contentWidth }
-              onChange={ ev => setAttributes({ contentWidth: ev.target.value }) } />
-          </BaseControl>
+            {/* Image placement */}
+            <SelectControl
+              label={ __('Image Placement') }
+              value={ imageLayout }
+              options={ [
+                {
+                  label: __('No Image'),
+                  value: '',
+                },
+                {
+                  label: __('On the left'),
+                  value: 'left',
+                },
+                {
+                  label: __('On the right'),
+                  value: 'right',
+                },
+              ] }
+              onChange={ value => setAttributes({ imageLayout: value }) }
+            />
 
-          {/* Image placement */}
-          <SelectControl
-            label={ __('Image Placement') }
-            value={ imageLayout }
-            options={ [
+            {/* Background control */}
+            <SelectControl
+              label={ __('Background Type') }
+              value={ backgroundType }
+              options={ [
+                {
+                  label: __('Solid Color'),
+                  value: 'color',
+                },
+                {
+                  label: __('Image'),
+                  value: 'image',
+                },
+              ] }
+              onChange={ value => setAttributes({ backgroundType: value }) }
+            />
+
+            { backgroundType === 'image' &&
+              <RangeControl
+                label={ __('Overlay Opacity') } value={ overlayOpacity }
+                onChange={ value => setAttributes({ overlayOpacity: value }) }
+                min={ 0 } max={ 100 } step={ 5 }
+              /> }
+          </PanelBody>
+
+          { backgroundType !== 'image' && <PanelColorSettings
+            title={ __('Color Settings') }
+            initialOpen={ true }
+            colorSettings={ [
               {
-                label: __('No Image'),
-                value: '',
+                value: backgroundColor,
+                onChange: value => setAttributes({ backgroundColor: value }),
+                label: __('Background Color'),
               },
-              {
-                label: __('On the left'),
-                value: 'left',
-              },
-              {
-                label: __('On the right'),
-                value: 'right',
-              },
-            ] }
-            onChange={ value => setAttributes({ imageLayout: value }) }
-          />
-
-          {/* Background control */}
-          <SelectControl
-            label={ __('Background Type') }
-            value={ backgroundType }
-            options={ [
-              {
-                label: __('Solid Color'),
-                value: 'color',
-              },
-              {
-                label: __('Image'),
-                value: 'image',
-              },
-            ] }
-            onChange={ value => setAttributes({ backgroundType: value }) }
-          />
-
-          { backgroundType === 'image' &&
-            <RangeControl
-              label={ __('Overlay Opacity') } value={ overlayOpacity }
-              onChange={ value => setAttributes({ overlayOpacity: value }) }
-              min={ 0 } max={ 100 } step={ 5 }
-            /> }
-        </PanelBody>
-
-        { backgroundType !== 'image' && <PanelColorSettings
-          title={ __('Color Settings') }
-          initialOpen={ true }
-          colorSettings={ [
-            {
-              value: backgroundColor,
-              onChange: value => setAttributes({ backgroundColor: value }),
-              label: __('Background Color'),
-            },
-          ]}></PanelColorSettings>
-        }
-      </InspectorControls>,
-    ];
+            ] }></PanelColorSettings>
+          }
+        </InspectorControls>
+      </Fragment>
+    );
   },
 
   save ({ attributes, className }) {
